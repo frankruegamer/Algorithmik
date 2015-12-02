1b6314a0b8f3de36547daed34c9523625352e950
frankruegamer <frankruegamer@gmail.com>
2015-12-02 15:25:18 +0100

    Fix for numbers with just one digit and server version of 'primzahlen'

--- T2-G1.rkt
+++ T2-G1.rkt
@@ -10,11 +10,22 @@
     (if (> n (- (length l) 2)) 0
         (let ((m (+ n 1)))
           (+ (if (op (list-ref l n) (list-ref l m)) 1 0) (scan-list l (+ n 1))))))
-  (scan-list (make-list (abs zahl)) 0))
+  (if (= (quotient zahl 10) 0) #f
+      (scan-list (make-list (abs zahl)) 0)))
 
 ; Aufgabe 2
 (define (primzahlen f)
   (define (primzahlen-iter n)
     (if (not (prime? (f n))) (if (= n 0) #f (- n 1))
+        (primzahlen-iter (+ n 1))))
+  (primzahlen-iter 0))
+
+; Version für Algo-Server, da (prime? 0) und (prime? 1) dort #t
+(define (primzahlen-server f)
+  (define (prime-fix? n)
+    (if (< n 2) #f
+        (prime? n)))
+  (define (primzahlen-iter n)
+    (if (not (prime-fix? (f n))) (if (= n 0) #f (- n 1))
         (primzahlen-iter (+ n 1))))
   (primzahlen-iter 0))
